d8a5e2e22499cc3744b241dd62bfc93c0ca85a1f
Mikey Zhong <mzhong21@gmail.com>
2025-03-20 15:19:30 -0400

    Change ReactRough to be able to accept children as a prop.

--- dist/components/ReactRough/ReactRough.d.ts
+++ dist/components/ReactRough/ReactRough.d.ts
@@ -1,6 +1,14 @@
 import React from "react";
-declare function ReactRough(props: object): React.JSX.Element;
-declare namespace ReactRough {
-    var displayName: string;
+
+interface ReactRoughProps {
+  children?: React.ReactNode;
+  [key: string]: any; // For any additional props
 }
+
+declare function ReactRough(props: ReactRoughProps): React.JSX.Element;
+
+declare namespace ReactRough {
+  var displayName: string;
+}
+
 export default ReactRough;
